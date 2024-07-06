aa0c43384ff3161027377e76fda5d7b664879271
S-Kathirvel <kathirvel.shanmugam.s@gmail.com>
2024-07-07 00:27:06 +0530

    Submitted on 06/07/2024

--- 1492_Kth_Factor.py
+++ 1492_Kth_Factor.py
@@ -1,10 +1,12 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         factors = []
-        for i in range(1, n+1):
+        for i in range(1, int(sqrt(n))+1):
             if n % i == 0:
                 factors.append(i)
-
+                if i != n//i:
+                    factors.append(n//i)
+        factors.sort()
         if len(factors) < k:
             return -1
         else:
