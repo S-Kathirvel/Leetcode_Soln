3ac1e243c7d1a151e6f1ab29065f9561f02bf9d7
S-Kathirvel <kathirvel.shanmugam.s@gmail.com>
2024-07-06 20:24:13 +0530

    Submitted on 06/07/2024

---
+++ 6_Zigzag_Conversion.py
@@ -0,0 +1,13 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        rows = [''] * numRows
+        direction = -1
+        k = 0
+
+        for char in s:
+            rows[k] += char
+            if k==0 or k==(numRows - 1): # Check if Nth row is reached
+                direction *= -1 # if so reversing the direction so text can be added in zigzag
+            k += direction # k value will revolve around 0-2 (for numRows = 3)
+
+        return ''.join(rows)
